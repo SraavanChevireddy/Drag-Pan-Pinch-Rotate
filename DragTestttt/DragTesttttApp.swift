e2f061e369ee60f32bca30b9871954dced9bdcde
Sraavan Chevireddy <63583829+SraavanChevireddy@users.noreply.github.com>
2022-08-30 18:39:03 +0530

    Initial Commit

---
+++ DragTestttt/DragTesttttApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragTesttttApp.swift
+//  DragTestttt
+//
+//  Created by Sraavan Chevireddy on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DragTesttttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
